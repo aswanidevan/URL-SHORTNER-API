8d0d335f41ef380d200f3c019a7e8cc28006d6b7
aswanidevan <66940827+aswanidevan@users.noreply.github.com>
2024-05-17 15:33:04 +0530

    Support multiple CORS origin

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ var cors = require('cors');
 
 const app=express();
 const PORT=process.env.PORT ||3000;
-const allowedOrigins = [ process.env.APP_URL, process.env.APP_URL-1, process.env.APP_URL-2];
+const allowedOrigins = [ process.env.APP_URL, process.env.APP_URL_1, process.env.APP_URL_2];
 //MIDDLEWARE
 
 app.use(express.urlencoded({ extended: true }));
